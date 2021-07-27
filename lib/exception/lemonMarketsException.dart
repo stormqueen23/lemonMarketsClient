cc1935124b5c96327633b345b4738605fc95171b
Melanie Siekmöller <develop@preiswaechter.com>
2021-07-27 07:35:00 +0200

    show runtimeType of exception

--- lib/exception/lemonMarketsException.dart
+++ lib/exception/lemonMarketsException.dart
@@ -9,6 +9,6 @@ class LemonMarketsException implements Exception {
 
   @override
   String toString() {
-    return 'LemonMarketsException{url: $url, responseCode: $responseCode, responseMap: $responseMap, rootCause: $rootCause}';
+    return '${this.runtimeType}{url: $url, responseCode: $responseCode, responseMap: $responseMap, rootCause: $rootCause}';
   }
 }
