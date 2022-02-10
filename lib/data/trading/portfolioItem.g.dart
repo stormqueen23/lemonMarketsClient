7a6dbf85ea524e337280d591e9fcb2a41759c707
stormqueen <melanie@siekmoeller.com>
2022-02-10 12:45:32 +0100

    adapt to new portfolio endpoint

--- lib/data/trading/portfolioItem.g.dart
+++ lib/data/trading/portfolioItem.g.dart
@@ -12,25 +12,8 @@ PortfolioItem _$PortfolioItemFromJson(Map<String, dynamic> json) =>
       json['isin_title'] as String,
       json['space_id'] as String,
       json['quantity'] as int,
-      json['buy_quantity'] as int?,
-      json['sell_quantity'] as int?,
       LemonMarketsAmountConverter.fromNullableAmount(
           json['buy_price_avg'] as num?),
-      LemonMarketsAmountConverter.fromNullableAmount(
-          json['buy_price_min'] as num?),
-      LemonMarketsAmountConverter.fromNullableAmount(
-          json['buy_price_max'] as num?),
-      LemonMarketsAmountConverter.fromNullableAmount(
-          json['buy_price_avg_historical'] as num?),
-      LemonMarketsAmountConverter.fromNullableAmount(
-          json['sell_price_min'] as num?),
-      LemonMarketsAmountConverter.fromNullableAmount(
-          json['sell_price_max'] as num?),
-      LemonMarketsAmountConverter.fromNullableAmount(
-          json['sell_price_avg_historical'] as num?),
-      json['orders_total'] as int?,
-      json['sell_orders_total'] as int?,
-      json['buy_orders_total'] as int?,
     );
 
 Map<String, dynamic> _$PortfolioItemToJson(PortfolioItem instance) =>
@@ -39,23 +22,6 @@ Map<String, dynamic> _$PortfolioItemToJson(PortfolioItem instance) =>
       'isin_title': instance.isinTitle,
       'space_id': instance.spaceUuid,
       'quantity': instance.quantity,
-      'buy_quantity': instance.quantityBuy,
-      'sell_quantity': instance.quantitySell,
       'buy_price_avg':
           LemonMarketsAmountConverter.toNullableAmount(instance.buyPriceAvg),
-      'buy_price_min':
-          LemonMarketsAmountConverter.toNullableAmount(instance.buyPriceMin),
-      'buy_price_max':
-          LemonMarketsAmountConverter.toNullableAmount(instance.buyPriceMax),
-      'buy_price_avg_historical': LemonMarketsAmountConverter.toNullableAmount(
-          instance.buyPriceAvgHistorical),
-      'sell_price_min':
-          LemonMarketsAmountConverter.toNullableAmount(instance.sellPriceMin),
-      'sell_price_max':
-          LemonMarketsAmountConverter.toNullableAmount(instance.sellPriceMax),
-      'sell_price_avg_historical': LemonMarketsAmountConverter.toNullableAmount(
-          instance.sellPriceAvgHistorical),
-      'orders_total': instance.ordersTotal,
-      'sell_orders_total': instance.sellOrdersTotal,
-      'buy_orders_total': instance.buyOrdersTotal,
     };
